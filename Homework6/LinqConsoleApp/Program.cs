9b397199a2320f0a1e3bc699184084ffe5b04976
Ethan Penrose <aw5680@live.missouristate.edu>
2021-05-02 22:28:09 -0500

    Added homework 6 assignment.

---
+++ Homework6/LinqConsoleApp/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace LinqConsoleApp
+{
+    using System;
+    using System.Data.Linq;
+    using System.Data.Linq.Mapping;
+    using System.Linq;
+
+    public class Northwind : DataContext
+    {
+        // Table<T> abstracts database details per table/data type.
+        public Table<Customer> Customers;
+        public Table<Order> Orders;
+
+        public Northwind(string connection) : base(connection) { }
+    }
+
+    [Table(Name = "Customers")]
+    public class Customer
+    {
+        private string _CustomerID;
+        [Column(IsPrimaryKey=true, Storage="_CustomerID")]
+        public string CustomerID
+        {
+            get
+            {
+                return this._CustomerID;
+            }
+            set
+            {
+                this._CustomerID = value;
+            }
+        }
+
+        private string _City;
+        [Column(Storage="_City")]
+        public string City
+        {
+            get
+            {
+                return this._City;
+            }
+            set
+            {
+                this._City=value;
+            }
+        }
+
+        private EntitySet<Order> _Orders;
+        public Customer()
+        {
+            this._Orders = new EntitySet<Order>();
+        }
+
+        [Association(Storage = "_Orders", OtherKey = "CustomerID")]
+        public EntitySet<Order> Orders
+        {
+            get { return this._Orders; }
+            set { this._Orders.Assign(value); }
+        }
+    }
+
+    [Table(Name = "Orders")]
+    public class Order
+    {
+        private int _OrderID = 0;
+        private string _CustomerID;
+        private EntityRef<Customer> _Customer;
+        public Order() { this._Customer = new EntityRef<Customer>(); }
+
+        [Column(Storage = "_OrderID", DbType = "Int NOT NULL IDENTITY",
+            IsPrimaryKey = true, IsDbGenerated = true)]
+        public int OrderID
+        {
+            get { return this._OrderID; }
+            // No need to specify a setter because IsDBGenerated is
+            // true.
+        }
+
+        [Column(Storage = "_CustomerID", DbType = "NChar(5)")]
+        public string CustomerID
+        {
+            get { return this._CustomerID; }
+            set { this._CustomerID = value; }
+        }
+
+        [Association(Storage = "_Customer", ThisKey = "CustomerID")]
+        public Customer Customer
+        {
+            get { return this._Customer.Entity; }
+            set { this._Customer.Entity = value; }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Use a connection string.
+            Northwind db = new Northwind($"{Properties.Settings.Default.SqlDbFilePath}"
+                                         + $"\\{Properties.Settings.Default.SqlDbFileName}");
+
+            // Query for customers from Seattle.
+            var custQuery =
+                from cust in db.Customers
+                where cust.City == "Seattle"
+                select cust;
+
+            foreach (var custObj in custQuery)
+            {
+                Console.WriteLine("ID={0}", custObj.CustomerID);
+            }
+            // Freeze the console window.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework6/LinqDataManipulationApp/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace LinqDataManipulationApp
+{
+    using System;
+    using System.Data.Linq;
+    using System.Data.Linq.Mapping;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Use the following connection string.
+            Northwnd db = new Northwnd($"{Properties.Settings.Default.SqlDbFilePath}"
+                                       + $"\\{Properties.Settings.Default.SqlDbFileName}");
+
+            // Create the new Customer object.
+            Customer newCust = new Customer();
+            newCust.CompanyName = "AdventureWorks Cafe";
+            newCust.CustomerID = "ADVCA";
+
+            // Add the customer to the Customers table.
+            db.Customers.InsertOnSubmit(newCust);
+
+            Console.WriteLine("\nCustomers matching CA before insert");
+
+            foreach (var c in db.Customers.Where(cust => cust.CustomerID.Contains("CA")))
+            {
+                Console.WriteLine("{0}, {1}, {2}",
+                    c.CustomerID, c.CompanyName, c.Orders.Count);
+            }
+
+            // Query for specific customer.
+            // First() returns one object rather than a collection.
+            var existingCust =
+                (from c in db.Customers
+                 where c.CustomerID == "ALFKI"
+                 select c)
+                .First();
+
+            // Change the contact name of the customer.
+            existingCust.ContactName = "New Contact";
+
+            // Access the first element in the Orders collection.
+            Order ord0 = existingCust.Orders[0];
+
+            // Access the first element in the OrderDetails collection.
+            OrderDetail detail0 = ord0.OrderDetails[0];
+
+            // Display the order to be deleted.
+            Console.WriteLine
+            ("The Order Detail to be deleted is: OrderID = {0}, ProductID = {1}",
+                detail0.OrderID, detail0.ProductID);
+
+            // Mark the Order Detail row for deletion from the database.
+            db.OrderDetails.DeleteOnSubmit(detail0);
+
+            db.SubmitChanges();
+
+            Console.WriteLine("\nCustomers matching CA after update");
+            foreach (var c in db.Customers.Where(cust =>
+                cust.CustomerID.Contains("CA")))
+            {
+                Console.WriteLine("{0}, {1}, {2}",
+                    c.CustomerID, c.CompanyName, c.Orders.Count);
+            }
+
+            // Keep the console window open after activity stops.
+            Console.ReadLine();
+        }
+    }
+}
